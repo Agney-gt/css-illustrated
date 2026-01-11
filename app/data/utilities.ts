602390d0ce5ce67dd9683979977d943edddbcc9f
ojasvsingh71 <ojasvsingh0@gmail.com>
2026-01-11 21:54:27 +0530

    Added all the rest of the utils

--- app/data/utilities.ts
+++ app/data/utilities.ts
@@ -3,6 +3,10 @@ export const categories = [
     name: "Accessibility",
     utilities: [
       {
+        name: "Forced Color Adjust",
+        href: "/utilities-1/accessibility/forced-color-adjust",
+      },
+      {
         name: "Screen-readers",
         href: "/utilities-1/accessibility/screen-readers",
       },
@@ -14,7 +18,10 @@ export const categories = [
       { name: "Align Content", href: "/utilities-1/alignment/align/content" },
       { name: "Align Items", href: "/utilities-1/alignment/align/items" },
       { name: "Align Self", href: "/utilities-1/alignment/align/self" },
-      { name: "Justify Content", href: "/utilities-1/alignment/justify/content" },
+      {
+        name: "Justify Content",
+        href: "/utilities-1/alignment/justify/content",
+      },
       { name: "Justify Items", href: "/utilities-1/alignment/justify/items" },
       { name: "Justify Self", href: "/utilities-1/alignment/justify/self" },
       { name: "Place Content", href: "/utilities-1/alignment/place/content" },
@@ -25,12 +32,27 @@ export const categories = [
   {
     name: "Background",
     utilities: [
+      {
+        name: "Background Attachment",
+        href: "/utilities-1/background/background-attachment",
+      },
+      {
+        name: "Background Clip",
+        href: "/utilities-1/background/background-clip",
+      },
       { name: "Background Color", href: "/utilities-1/background/color" },
       { name: "Background Image", href: "/utilities-1/background/image" },
+      {
+        name: "Background Origin",
+        href: "/utilities-1/background/background-origin",
+      },
       { name: "Background Position", href: "/utilities-1/background/position" },
       { name: "Background Repeat", href: "/utilities-1/background/repeat" },
       { name: "Background Size", href: "/utilities-1/background/size" },
-      { name: "Gradient Stops", href: "/utilities-1/background/gradient-stops" },
+      {
+        name: "Gradient Stops",
+        href: "/utilities-1/background/gradient-stops",
+      },
     ],
   },
   {
@@ -47,6 +69,22 @@ export const categories = [
   {
     name: "Effects",
     utilities: [
+      {
+        name: "Background Blend Mode",
+        href: "/utilities-1/effects/background-blend-mode",
+      },
+      { name: "Box Shadow", href: "/utilities-1/effects/box-shadow" },
+      { name: "Mask Clip", href: "/utilities-1/effects/mask-clip" },
+      { name: "Mask Composite", href: "/utilities-1/effects/mask-composite" },
+      { name: "Mask Image", href: "/utilities-1/effects/mask-image" },
+      { name: "Mask Mode", href: "/utilities-1/effects/mask-mode" },
+      { name: "Mask Origin", href: "/utilities-1/effects/mask-origin" },
+      { name: "Mask Position", href: "/utilities-1/effects/mask-position" },
+      { name: "Mask Repeat", href: "/utilities-1/effects/mask-repeat" },
+      { name: "Mask Size", href: "/utilities-1/effects/mask-size" },
+      { name: "Mask Type", href: "/utilities-1/effects/mask-type" },
+      { name: "Mix Blend Mode", href: "/utilities-1/effects/mix-blend-mode" },
+      { name: "Opacity", href: "/utilities-1/effects/opacity" },
       { name: "Outline Color", href: "/utilities-1/effects/outline/color" },
       { name: "Outline Offset", href: "/utilities-1/effects/outline/offset" },
       { name: "Outline Style", href: "/utilities-1/effects/outline/style" },
@@ -60,11 +98,29 @@ export const categories = [
         name: "Ring Offset Width",
         href: "/utilities-1/effects/ring/offset-width",
       },
+      { name: "Text Shadow", href: "/utilities-1/effects/text-shadow" },
+    ],
+  },
+  {
+    name: "Filters",
+    utilities: [
+      { name: "Backdrop Filter", href: "/utilities-1/filters/backdrop-filter" },
+      { name: "Blur", href: "/utilities-1/filters/blur" },
+      { name: "Brightness", href: "/utilities-1/filters/brightness" },
+      { name: "Contrast", href: "/utilities-1/filters/contrast" },
+      { name: "Drop Shadow", href: "/utilities-1/filters/drop-shadow" },
+      { name: "Filter", href: "/utilities-1/filters/filter" },
+      { name: "Grayscale", href: "/utilities-1/filters/grayscale" },
+      { name: "Hue Rotate", href: "/utilities-1/filters/hue-rotate" },
+      { name: "Invert", href: "/utilities-1/filters/invert" },
+      { name: "Saturate", href: "/utilities-1/filters/saturate" },
+      { name: "Sepia", href: "/utilities-1/filters/sepia" },
     ],
   },
   {
     name: "Flexbox",
     utilities: [
+      { name: "Flex", href: "/utilities-1/flex/flex" },
       { name: "Flex Basis", href: "/utilities-1/flex/basis" },
       { name: "Flex Direction", href: "/utilities-1/flex/direction" },
       { name: "Flex Grow", href: "/utilities-1/flex/grow" },
@@ -72,6 +128,7 @@ export const categories = [
       { name: "Flex Shrink", href: "/utilities-1/flex/shrink" },
       { name: "Flex Sizing", href: "/utilities-1/flex/sizing" },
       { name: "Flex Wrap", href: "/utilities-1/flex/wrap" },
+      { name: "Order", href: "/utilities-1/flex/order" },
     ],
   },
   {
@@ -80,6 +137,16 @@ export const categories = [
       { name: "Grid Auto Columns", href: "/utilities-1/grid/auto-columns" },
       { name: "Grid Auto Flow", href: "/utilities-1/grid/auto-flow" },
       { name: "Grid Auto Rows", href: "/utilities-1/grid/auto-rows" },
+      { name: "Grid Column", href: "/utilities-1/grid/grid-column" },
+      { name: "Grid Row", href: "/utilities-1/grid/grid-row" },
+      {
+        name: "Grid Template Columns",
+        href: "/utilities-1/grid/grid-template-columns",
+      },
+      {
+        name: "Grid Template Rows",
+        href: "/utilities-1/grid/grid-template-rows",
+      },
     ],
   },
   {
@@ -97,10 +164,13 @@ export const categories = [
       },
       { name: "Resize", href: "/utilities-1/interactivity/resize" },
       {
-        name: "Scroll Behaviour",
-        href: "/utilities-1/interactivity/scroll-behaviour",
+        name: "Scroll Behavior",
+        href: "/utilities-1/interactivity/scroll-behavior",
       },
-      { name: "Scroll Margin", href: "/utilities-1/interactivity/scroll-margin" },
+      {
+        name: "Scroll Margin",
+        href: "/utilities-1/interactivity/scroll-margin",
+      },
       {
         name: "Scroll Padding",
         href: "/utilities-1/interactivity/scroll-padding",
@@ -125,23 +195,45 @@ export const categories = [
   {
     name: "Layout",
     utilities: [
+      { name: "Aspect Ratio", href: "/utilities-1/layout/aspect-ratio" },
       { name: "Box", href: "/utilities-1/layout/box" },
+      {
+        name: "Box Decoration Break",
+        href: "/utilities-1/layout/box-decoration-break",
+      },
+      { name: "Box Sizing", href: "/utilities-1/layout/box-sizing" },
       { name: "Break After", href: "/utilities-1/layout/break-after" },
       { name: "Break Before", href: "/utilities-1/layout/break-before" },
       { name: "Break Inside", href: "/utilities-1/layout/break-inside" },
+      { name: "Clear", href: "/utilities-1/layout/clear" },
       { name: "Columns", href: "/utilities-1/layout/columns" },
       { name: "Display", href: "/utilities-1/layout/display" },
+      { name: "Float", href: "/utilities-1/layout/float" },
       { name: "Floats", href: "/utilities-1/layout/floats" },
+      { name: "Isolation", href: "/utilities-1/layout/isolation" },
       { name: "Object Fit", href: "/utilities-1/layout/object/fit" },
       { name: "Object Position", href: "/utilities-1/layout/object/position" },
       { name: "Overflow", href: "/utilities-1/layout/overflow" },
+      {
+        name: "Overscroll Behavior",
+        href: "/utilities-1/layout/overscroll-behavior",
+      },
+      { name: "Position", href: "/utilities-1/layout/position" },
+      {
+        name: "Top / Right / Bottom / Left",
+        href: "/utilities-1/layout/top-right-bottom-left",
+      },
+      { name: "Visibility", href: "/utilities-1/layout/visibility" },
+      { name: "Z-Index", href: "/utilities-1/layout/z-index" },
     ],
   },
   {
     name: "Sizing",
     utilities: [
       { name: "Height", href: "/utilities-1/sizing/height" },
+      { name: "Max Height", href: "/utilities-1/sizing/max-height" },
       { name: "Max Width", href: "/utilities-1/sizing/max-width" },
+      { name: "Min Height", href: "/utilities-1/sizing/min-height" },
       { name: "Min Width", href: "/utilities-1/sizing/min-width" },
       { name: "Width", href: "/utilities-1/sizing/width" },
     ],
@@ -155,22 +247,152 @@ export const categories = [
     ],
   },
   {
+    name: "SVG",
+    utilities: [
+      { name: "Fill", href: "/utilities-1/svg/fill" },
+      { name: "Stroke", href: "/utilities-1/svg/stroke" },
+      { name: "Stroke Width", href: "/utilities-1/svg/stroke-width" },
+    ],
+  },
+  {
+    name: "Tables",
+    utilities: [
+      { name: "Border Collapse", href: "/utilities-1/tables/border-collapse" },
+      { name: "Border Spacing", href: "/utilities-1/tables/border-spacing" },
+      { name: "Caption Side", href: "/utilities-1/tables/caption-side" },
+      { name: "Table Layout", href: "/utilities-1/tables/table-layout" },
+    ],
+  },
+  {
     name: "Transform",
     utilities: [
+      {
+        name: "Backface Visibility",
+        href: "/utilities-1/transform/backface-visibility",
+      },
       { name: "Origin", href: "/utilities-1/transform/origin" },
+      { name: "Perspective", href: "/utilities-1/transform/perspective" },
+      {
+        name: "Perspective Origin",
+        href: "/utilities-1/transform/perspective-origin",
+      },
       { name: "Rotate", href: "/utilities-1/transform/rotate" },
       { name: "Scale", href: "/utilities-1/transform/scale" },
       { name: "Skew", href: "/utilities-1/transform/skew" },
+      { name: "Transform", href: "/utilities-1/transform/transform" },
+      {
+        name: "Transform Origin",
+        href: "/utilities-1/transform/transform-origin",
+      },
+      {
+        name: "Transform Style",
+        href: "/utilities-1/transform/transform-style",
+      },
       { name: "Translate", href: "/utilities-1/transform/translate" },
+    ],
+  },
+  {
+    name: "Transitions & Animation",
+    utilities: [
+      {
+        name: "Animation",
+        href: "/utilities-1/transitions-animation/animation",
+      },
+      {
+        name: "Transition Behavior",
+        href: "/utilities-1/transitions-animation/transition-behavior",
+      },
+      {
+        name: "Transition Delay",
+        href: "/utilities-1/transitions-animation/transition-delay",
+      },
+      {
+        name: "Transition Duration",
+        href: "/utilities-1/transitions-animation/transition-duration",
+      },
+      {
+        name: "Transition Property",
+        href: "/utilities-1/transitions-animation/transition-property",
+      },
+      {
+        name: "Transition Timing Function",
+        href: "/utilities-1/transitions-animation/transition-timing-function",
+      },
     ],
   },
   {
     name: "Typography",
     utilities: [
+      { name: "Color", href: "/utilities-1/typography/color" },
+      { name: "Content", href: "/utilities-1/typography/content" },
       { name: "Font Family", href: "/utilities-1/typography/font-family" },
       { name: "Font Size", href: "/utilities-1/typography/font-size" },
+      {
+        name: "Font Smoothing",
+        href: "/utilities-1/typography/font-smoothing",
+      },
+      { name: "Font Stretch", href: "/utilities-1/typography/font-stretch" },
+      { name: "Font Style", href: "/utilities-1/typography/font-style" },
+      {
+        name: "Font Variant Numeric",
+        href: "/utilities-1/typography/font-variant-numeric",
+      },
       { name: "Font Weight", href: "/utilities-1/typography/font-weight" },
+      { name: "Hyphens", href: "/utilities-1/typography/hyphens" },
+      {
+        name: "Letter Spacing",
+        href: "/utilities-1/typography/letter-spacing",
+      },
+      { name: "Line Clamp", href: "/utilities-1/typography/line-clamp" },
+      { name: "Line Height", href: "/utilities-1/typography/line-height" },
+      {
+        name: "List Style Image",
+        href: "/utilities-1/typography/list-style-image",
+      },
+      {
+        name: "List Style Position",
+        href: "/utilities-1/typography/list-style-position",
+      },
+      {
+        name: "List Style Type",
+        href: "/utilities-1/typography/list-style-type",
+      },
+      { name: "Overflow Wrap", href: "/utilities-1/typography/overflow-wrap" },
+      { name: "Text Align", href: "/utilities-1/typography/text-align" },
       { name: "Text Color", href: "/utilities-1/typography/font-color" },
+      {
+        name: "Text Decoration Color",
+        href: "/utilities-1/typography/text-decoration-color",
+      },
+      {
+        name: "Text Decoration Line",
+        href: "/utilities-1/typography/text-decoration-line",
+      },
+      {
+        name: "Text Decoration Style",
+        href: "/utilities-1/typography/text-decoration-style",
+      },
+      {
+        name: "Text Decoration Thickness",
+        href: "/utilities-1/typography/text-decoration-thickness",
+      },
+      { name: "Text Indent", href: "/utilities-1/typography/text-indent" },
+      { name: "Text Overflow", href: "/utilities-1/typography/text-overflow" },
+      {
+        name: "Text Transform",
+        href: "/utilities-1/typography/text-transform",
+      },
+      {
+        name: "Text Underline Offset",
+        href: "/utilities-1/typography/text-underline-offset",
+      },
+      { name: "Text Wrap", href: "/utilities-1/typography/text-wrap" },
+      {
+        name: "Vertical Align",
+        href: "/utilities-1/typography/vertical-align",
+      },
+      { name: "White Space", href: "/utilities-1/typography/white-space" },
+      { name: "Word Break", href: "/utilities-1/typography/word-break" },
     ],
   },
 ];
